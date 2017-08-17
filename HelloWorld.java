602441f7e0c2a1bcc6364c66c51dcfed905a2eae
Eyal Amram <amram.eyal@gmail.com>
2017-08-18 01:18:48 +0300

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Line here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+    public static void main(String[]args)
+    {
+        //output to user
+        System.out.println("Say Hello");
+    }
+}
